ade4cdfb122f813e3e10db166d3626050701d1f9
Khushi Kumari <khushii8969@gmail.com>
2025-03-30 12:34:11 +0530

    Q1
    "Added solution for Two Sum (LeetCode 1)"

---
+++ Duplicate_Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+      int slow = nums[0]; //at start position
+      int fast = nums[0]; //at start position
+      slow = nums[slow];  //only 1 step
+      fast = nums[nums[fast]]; // only 2 step
+      while(slow!=fast){  // 1st POI
+           slow = nums[slow];  //only 1 step ahead
+           fast = nums[nums[fast]];  //only 2 step ahead
+      }
+      fast = nums[0];   //again initializes fast to '0'
+      while(slow!=fast){ //2nd POI
+           slow = nums[slow];  // 1 step ahead
+           fast = nums[fast];   //1 step ahead
+      }
+      return slow;
+    }
+}
