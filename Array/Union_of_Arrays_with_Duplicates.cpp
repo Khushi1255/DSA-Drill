f612a0e6887c72d912b804103a8e127a76ab89cc
Khushi Kumari <khushii8969@gmail.com>
2025-05-24 05:45:23 +0530

    q24

---
+++ Array/Union_of_Arrays_with_Duplicates.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    // Function to return the count of number of elements in union of two arrays.
+    int findUnion(vector<int>& a, vector<int>& b) {
+        // code here
+        int n = a.size();
+        int m = b.size();
+        unordered_set<int>st;
+        for(int i=0;i<n;i++)  st.insert(a[i]);
+        for(int i=0;i<m;i++)  st.insert(b[i]);
+        return st.size();
+    }
+};
