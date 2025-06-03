837d6c66315bc0591a5adb404882dfc27dd7b8e3
Khushi Kumari <khushii8969@gmail.com>
2025-06-03 23:15:24 +0530

    q3

---
+++ Strings/Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);        
+    }
+};
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substr(i, needle.length()) == needle) {
+                return i;
+            }
+        }
+        
+        return -1;        
+    }
+};
